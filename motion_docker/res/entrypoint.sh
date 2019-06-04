0694e569250fd97421b94d7828580148ae83216e
Edward Beech <initialed85@gmail.com>
2019-06-04 17:37:11 +0800

    Moving some stuff

--- motion_docker/res/entrypoint.sh
+++ motion_docker/res/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+ARGS="-n"
+if [[ "${1}" == "no_motion" ]]; then
+    ARGS="-n -m"
+fi
+
 echo "instance uuid is ${INSTANCE_UUID}"
 
 RUN_UUID=$(uuidgen)
@@ -8,4 +13,4 @@ echo "${RUN_UUID}" > /srv/run.uuid
 
 echo "run uuid is ${RUN_UUID}"
 
-motion -n
+motion ${ARGS}
--- motion_docker/run.sh
+++ motion_docker/run.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+SUFFIX=""
+if [[ "${1}" != "" ]]; then
+    SUFFIX="-${1}"
+fi
+
 IMAGE=motion-cctv
 
-TAG=${IMAGE}-run
+CONTAINER=${IMAGE}-run${SUFFIX}
 
 docker run -it --restart=always --log-opt max-size=10m \
-    --name ${TAG} \
+    --name ${CONTAINER} \
     -p 8080:8080 \
     -p 8081:8081 \
     -v `pwd`/configs:/etc/motion \
     -v `pwd`/target_dir:/srv/target_dir \
-    ${IMAGE}
+    ${IMAGE} ${2} ${3}
 
-docker rm -f ${TAG}
+docker rm -f ${CONTAINER}
