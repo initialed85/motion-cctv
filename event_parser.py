e16e5faff22b88f144db43fafa47422b7af3b9f5
Edward Beech <initialed85@gmail.com>
2019-06-05 20:53:04 +0800

    Some changes to event_parser.py

--- event_parser.py
+++ event_parser.py
@@ -5,7 +5,9 @@ import sys
 from itertools import zip_longest
 
 _TARGET_DIR = '/media/storage/Cameras'
+
 _BROWSE_URL_PREFIX = '/browse/'
+
 _STYLE_SHEET = """
 BODY {
     font-family: Tahoma;
@@ -29,6 +31,7 @@ TD {
     border: 1px solid gray; 
 }
 """
+
 _HTML_TEMPLATE = """</html>
 <head>
 <title>Events as at {}</title>
@@ -41,13 +44,14 @@ _HTML_TEMPLATE = """</html>
 <h1>Events as at {}</h1>
 
 <center>
-<table width="80%">
+<table width="90%">
 
 <tr>
 <th>Event ID</th>
 <th>Camera ID</th>
 <th>Timestamp</th>
 <th>Duration</th>
+<th>Size</th>
 <th>Camera</th>
 <th>Screenshot</th>
 <th>Clip</th>
@@ -61,7 +65,9 @@ _HTML_TEMPLATE = """</html>
 </body>
 </html>
 """
+
 _HTML_REPEATER = """<tr>
+<td>{}</td>
 <td>{}</td>
 <td>{}</td>
 <td>{}</td>
@@ -72,22 +78,41 @@ _HTML_REPEATER = """<tr>
 </tr>"""
 
 
-def _get_files(extension):
+def get_movies():
     files = []
     for file_name in os.listdir(_TARGET_DIR):
         path = os.path.join(_TARGET_DIR, file_name)
         if not os.path.isfile(path):
             continue
 
-        if not file_name.endswith(extension):
+        if file_name.startswith('.'):
+            continue
+
+        if not file_name.endswith('.mkv'):
             continue
 
         stat = os.stat(path)
 
+        try:
+            created = datetime.datetime.fromtimestamp(stat.st_birthtime)
+        except Exception:
+            created = None
+
+        try:
+            modified = datetime.datetime.fromtimestamp(stat.st_mtime)
+        except Exception:
+            modified = None
+
+        try:
+            file_size = round(stat.st_size / 1000000, 2)
+        except Exception:
+            file_size = None
+
         files += [
             (
-                datetime.datetime.fromtimestamp(stat.st_birthtime),
-                datetime.datetime.fromtimestamp(stat.st_mtime),
+                created,
+                modified,
+                file_size,
                 file_name,
             )
         ]
@@ -95,12 +120,10 @@ def _get_files(extension):
     return files
 
 
-def get_movies():
-    return _get_files('.mkv')
-
-
 def get_pictures():
-    return [x for x in os.listdir(_TARGET_DIR) if x.endswith('.jpg')]
+    return [
+        x for x in os.listdir(_TARGET_DIR) if x.endswith('.jpg') and not x.startswith('.')
+    ]
 
 
 movies = get_movies()
@@ -108,8 +131,8 @@ movies = get_movies()
 pictures = get_pictures()
 
 events = []
-for (created, last_modified, movie), picture in zip_longest(movies, pictures):
-    duration = last_modified - created
+for (created, last_modified, file_size, movie), picture in zip_longest(movies, pictures):
+    duration = last_modified - created if created is not None not in [last_modified, created] else 'unknown'
 
     match = re.search(
         '(\d+)__(\d+)__(\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d)__(.*)\.',
@@ -128,9 +151,10 @@ for (created, last_modified, movie), picture in zip_longest(movies, pictures):
         camera_id,
         timestamp,
         duration,
+        '{} MB'.format(file_size) if file_size is not None else 'unknown',
         camera_name,
-        '{}{}'.format(_BROWSE_URL_PREFIX, movie),
-        '{}{}'.format(_BROWSE_URL_PREFIX, picture)
+        '{}{}'.format(_BROWSE_URL_PREFIX, picture),
+        '{}{}'.format(_BROWSE_URL_PREFIX, movie)
     ]]
 
 repeaters = []
@@ -146,3 +170,4 @@ with open(sys.argv[1], 'w') as f:
         now,
         '\n\n'.join(repeaters)
     ))
+
